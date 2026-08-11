02072873be99a59cb00a1e708bf16e6eed53eb61
Md Waqar Moid <waqarmoid2@gmail.com>
2026-08-11 22:01:27 +0530

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+    vector<int> inDegree(n + 1, 0);
+        vector<int> outDegree(n + 1, 0);
+
+        // Build edge counts from trust relationships
+        for (auto& t : trust) {
+            int a = t[0];
+            int b = t[1];
+            
+            outDegree[a]++; // 'a' trusts someone
+            inDegree[b]++;  // 'b' is trusted by someone
+        }
+
+        // Judge must have inDegree == n - 1 and outDegree == 0
+        for (int i = 1; i <= n; ++i) {
+            if (inDegree[i] == n - 1 && outDegree[i] == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }   
+    
+};
